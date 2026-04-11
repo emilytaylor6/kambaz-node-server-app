6387a0d5cbdb1f856205fec50994e1201a6c572d
Emily Taylor <emilytaylorr22@gmail.com>
2026-04-11 11:23:34 -0400

    Completed 6.4.2: Added modules to database.

--- kambaz/courses/dao.js
+++ kambaz/courses/dao.js
@@ -3,12 +3,12 @@ import model from "./model.js";
 
 export default function CoursesDao(db) {
   function findAllCourses() {
-    return model.find();
+    return model.find({}, { name: 1, description: 1 });
   }
 
   async function findCoursesForEnrolledUser(userId) {
     const { enrollments } = db;
-    const courses = await model.find();
+    const courses = await model.find({}, { name: 1, description: 1 });
     const enrolledCourses = courses.filter((course) =>
       enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
     return enrolledCourses;
--- kambaz/courses/schema.js
+++ kambaz/courses/schema.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import moduleSchema from "../modules/schema.js";
 
 const courseSchema = new mongoose.Schema({
    _id: String,
    name: String,
    number: String,
+   startDate: Date,
+   endDate: Date,
+   department: String,
    credits: Number,
    description: String,
+   image: String,
+   modules: [moduleSchema],
  },
  { collection: "courses" }
 );
